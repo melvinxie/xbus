92a80ca702eb11b3bf7399bf11aa33b8c67ce27e
Mingmin Xie <melvinxie@gmail.com>
2011-01-08 21:32:53 +0900

    template translation

--- appengine/settings.py
+++ appengine/settings.py
@@ -27,7 +27,7 @@ TIME_ZONE = 'Asia/Tokyo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
 SITE_ID = 1
 
--- appengine/timetables/views.py
+++ appengine/timetables/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render_to_response
 from django.utils import translation
 
 def index(request):
-    translation.activate('ja-jp')
+    translation.activate('en')
     return render_to_response('index.html', {'mobile': request.mobile})
